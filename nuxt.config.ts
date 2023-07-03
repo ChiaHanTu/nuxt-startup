493df0238044bda9363485a1f9f641a84a8df7a7
hans <hans@twjoin.com>
2023-07-03 11:22:45 +0800

    chore: adjust nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,7 @@ export default defineNuxtConfig({
       title: "nuxt-startup",
       viewport: "width=device-width,initial-scale=1",
       link: [{ rel: "icon", href: "/favicon.ico", sizes: "any" }],
-      meta: [
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "description", content: "nuxt-startup" },
-      ],
+      meta: [{ name: "description", content: "nuxt-startup" }],
     },
   },
   modules: ["@unocss/nuxt", "@pinia/nuxt", "@vueuse/nuxt"],
