1b8058a575ad29a1c3ea4546f2a239bc422506fc
hans <hans@twjoin.com>
2023-07-03 10:30:50 +0800

    feat(directory): update

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>You're offline</div>
+  <div class="index-page">
+    You're offline
+  </div>
 </template>
+
+<style lang="scss">
+.index-page {
+  @apply text-center;
+}
+</style>
